715dad087be6f7b239211e49d16154c25bc9bdf1
loke <loke.oberg@elev.ga.ntig.se>
2023-09-05 14:26:28 +0200

    tests

---
+++ variabler.js
@@ -0,0 +1,9 @@
+let name = "Loke"
+console.log(typeof(name))
+const greeting = `Hej ${name}`
+console.log(greeting)
+let age = 19
+console.log(typeof(age))
+let x = Number.MAX_VALUE
+console.log(typeof(x+x))
+console.log(isNaN(x+x))
